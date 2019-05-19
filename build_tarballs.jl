1ce023df96eada2531dd07be81cc4c7a7e42c311
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-19 08:57:16 +0000

    Snapshot of branch master taken on 2019-05-19T04:57:15.47-04:00 from "https://github.com/UnofficialJuliaMirror/JuliaDatabases-SQLiteBuilder"

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,22 +2,20 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "SQLiteBuilder"
-version = v"0.1.0"
+name = "SQLite"
+version = v"3.28.0"
 
-# Collection of sources required to build SQLiteBuilder
+# Collection of sources required to build SQLite
 sources = [
-    "https://www.sqlite.org/2018/sqlite-autoconf-3240000.tar.gz" =>
-    "d9d14e88c6fb6d68de9ca0d1f9797477d82fc3aed613558f87ffbdbbc5ceb74a",
-
+    "https://www.sqlite.org/2019/sqlite-autoconf-3280000.tar.gz" =>
+    "d61b5286f062adfce5125eaf544d495300656908e61fca143517afcc0a89b7c3",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd sqlite-autoconf-3240000/
+cd sqlite-autoconf-3280000/
 ./configure --prefix=$prefix --host=$target && make && make install
-
 """
 
 # These are the platforms we will build for by default, unless further
@@ -30,10 +28,7 @@ products(prefix) = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
-    
-]
+dependencies = []
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
